11c0690d83af6c5547227a45532a0e4680e90257
Brett Bailey <brett.b508@stu.nebo.edu>
2021-09-07 09:17:48 -0600

    Added more Tank Movement

--- Prototype 1 - Tank Sim/Assets/Level 1 Game Art/Scripts/PlayerController.cs
+++ Prototype 1 - Tank Sim/Assets/Level 1 Game Art/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float turnSpeed;
+    // Left & Right Input
+    public float hInput;
+    // Forward & Backward Input
+    public float vInput;
     // projectile
 
     // Start is called before the first frame update
@@ -17,6 +21,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //Getting button press values for Horizontal & Vertical Inputs
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        // Makes the vehicle go Left & Right
+        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
+        // Makes the vehicle go Forward & Backward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
     }
 }
