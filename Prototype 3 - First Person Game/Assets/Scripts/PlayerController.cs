70306fc6a307564eedcb6ac1aa2a590ea219cb8d
Brett Bailey <brett.b508@stu.nebo.edu>
2021-10-08 09:19:11 -0600

    Added Rotation
    Introduced to Raycasting

--- Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
@@ -27,12 +27,41 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        CamLook();
+        if(Input.GetButtonDown("Jump"))
+            Jump();
     }
     void Move()
-    {
+    {   //Get Keyboard input with move speed
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
+        // Applying velocity/movement to the rigidbody
+        Vector3 dir = transform.right * x + transform.forward * z;
+        // Jump direction
+        dir = rb.velocity.y;
+        // Apply direction to camera movement
+        rb.velocity = dir;
+    }
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+    void Jump()
+    {
+        // Cast ray to ground
+        Ray ray = new Ray(transform.position, Vector3.down);
+        // Check Ray length to jump
+        if(Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    void CamLook()
+    {  
+        //Get mouse input so we can look around with the mouse
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        // Clamp/Limit the vertical rotation of the camera
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        // Applying the rotation to Camera
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
     }
 }
