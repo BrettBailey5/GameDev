4c2503fec59a9df8195f6aa0e3828b7ab17d79f7
Brett Bailey <brett.b508@stu.nebo.edu>
2021-11-22 09:04:34 -0700

    Added particle effect to bullet

--- Prototype 3 - First Person Game/Assets/Scripts/Bullet.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/Bullet.cs
@@ -8,6 +8,8 @@ public class Bullet : MonoBehaviour
     public float lifeTime;
     private float shootTime;
 
+    public GameObject hitParticle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,9 @@ public class Bullet : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
+        // Create particle effect on hit then destroy particle effect
+        GameObject obj = Instantiate(hitParticle, transform.position, Quaternion.identity);
+        Destroy(obj,1.0f);
         //Did we hit the player or the enemy
         if(other.CompareTag("Player"))
             other.GetComponent<PlayerController>().TakeDamage(damage);
